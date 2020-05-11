1209fdd1b67abff2eddb5afd3ab7922502810fb9
Bastien Docquois <bastien.docquois@gmail.com>
2020-05-11 15:54:00 +0200

    Added : PDF export support (albeit somewhat broken)

--- src/main/java/com/deepoove/poi/Main.java
+++ src/main/java/com/deepoove/poi/Main.java
@@ -2,14 +2,30 @@ package com.deepoove.poi;
 
 import com.deepoove.poi.data.PictureRenderData;
 import com.deepoove.poi.util.BytePictureUtils;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 
 public class Main {
+
+	public void convertToPDF(String docPath, String pdfPath) {
+		try {
+			InputStream doc = new FileInputStream(new File(docPath));
+			XWPFDocument document = new XWPFDocument(doc);
+			PdfOptions options = PdfOptions.create();
+			OutputStream out = new FileOutputStream(new File(pdfPath));
+			PdfConverter.getInstance().convert(document, out, options);
+			System.out.println("Done");
+		} catch (FileNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		} catch (IOException ex) {
+
+			System.out.println(ex.getMessage());
+		}
+	}
 	public static void main(String[] args) throws IOException {
 
 		// LIEN DU TUTO EN ANGLAIS
@@ -44,7 +60,18 @@ public class Main {
 		template.close();
 
 		// Convertir un .docx en PDF
+		// https://stackoverflow.com/questions/51440312/docx-to-pdf-converter-in-java
 
+		InputStream in = new FileInputStream(OUTPUT_DOCX);
+		XWPFDocument document = new XWPFDocument(in);
+		PdfOptions options = PdfOptions.create();
+		OutputStream pdf = new FileOutputStream(OUTPUT_PDF);
+		PdfConverter.getInstance().convert(document, pdf, options);
 
+		document.close();
+		out.close();
 	}
+
+
 }
+
